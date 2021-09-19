0d6febd188af96b0348372e35e6b436e8e9a6854
AnnabelNkir <annabel.micheni@student.moringaschool.com>
2021-09-19 14:04:42 +0300

    Add pizza type, crust,toppings contractors and prototypes

--- js/script.js
+++ js/script.js
@@ -10,3 +10,26 @@ function pizzaTypes(name, image, description) {
         "Small": 500
     }
 }
+pizzaTypes.prototype.price = 0;
+pizzaTypes.prototype.crust = null;
+pizzaTypes.prototype.topping = [];
+
+function crusts(name, price) {
+    this.name = name;
+    this.price = price;
+}
+
+function toppings(name, price) {
+    this.name = name;
+    this.price = price;
+}
+
+function Cart(){
+    const cart = this;
+    this.cartItems = [];
+    this.delivery = null;
+    this.addToCart = function(item){
+        cart.cartItems.push(item);
+        $("#cartItems").html(cartItems.length);
+    }
+}
